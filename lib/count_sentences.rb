c936127b9171e5d976e6e689d1c624d890f87955
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-22 00:27:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sent_array = self.split(/[.?!]/)
+    new_array = []
+    sent_array.each do |sent|
+      if !sent.empty?
+        new_array << sent
+      end
+    end
+    new_array.length
   end
 end
